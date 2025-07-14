2c51387ab5d388ad4be125a7aedff68033490b06
maton369 <ysk901085@gmail.com>
2025-07-14 17:35:54 +0900

    Add: DockerHubからイメージを削除

--- chapter01/exec.sh
+++ chapter01/exec.sh
@@ -2,4 +2,5 @@
 
 # docker build -t matondev/sample-web:intro .
 # docker images matondev/sample-web | tee output.log
-docker push matondev/sample-web:intro
+# docker push matondev/sample-web:intro
+docker rmi matondev/sample-web:intro
